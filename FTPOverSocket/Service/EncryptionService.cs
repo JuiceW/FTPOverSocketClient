1348fe3bcde2f40ac9d1fd2d61193a0f071fe326
Wang Guozhi <wgz615402729@live.com>
2019-09-14 13:53:29 -0400

    update

---
+++ FTPOverSocket/Service/EncryptionService.cs
@@ -0,0 +1,18 @@
+﻿using NSec.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPOverSocket.Service
+{
+    class EncryptionService
+    {
+        private static ChaCha20Poly1305 chacha20 { get; }
+        public EncryptionService()
+        {
+            
+        }
+    }
+}
